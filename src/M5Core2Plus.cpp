94f14a4276fbef9096abe2267fca05c54c113cef
Alrott SlimRG <slimrg@ya.ru>
2021-08-27 22:57:13 +0300

    Update

--- src/M5Core2Plus.cpp
+++ src/M5Core2Plus.cpp
@@ -1,5 +1,2 @@
-// Copyright (c) M5Core2. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 #include "M5Core2Plus.h"
 
--- src/M5Core2Plus.h
+++ src/M5Core2Plus.h
@@ -1,1 +1,6 @@
-#include <M5Core2>
+#ifndef _M5Core2Plus_H_
+  #define _M5Core2Plus_H_
+  
+  #include <M5Core2.h>
+  
+#endif
