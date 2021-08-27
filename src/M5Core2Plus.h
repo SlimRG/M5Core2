f1e6beb83467e082d5c16503fc2f1ffb07c62395
Alrott SlimRG <slimrg@ya.ru>
2021-08-27 23:40:07 +0300

    Update M5Core2Plus.h

--- src/M5Core2Plus.h
+++ src/M5Core2Plus.h
@@ -1,6 +1,22 @@
+ #define ESP32
+
 #ifndef _M5Core2Plus_H_
   #define _M5Core2Plus_H_
+
+  #if defined(ESP32)
   
   #include <M5Core2.h>
-  
+
+   class M5Core2Plus: : public M5Core2 {
+
+     //Power_Plus Power;
+
+
+   }
+
+   extern M5Core2Plus M5P;
+
+  #else
+    #error “This library only supports boards with ESP32 processor.”
+  #endif
 #endif
